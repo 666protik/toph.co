a94e6a62e251bffdf5ea33b678fab89558f92434
PROTIK ROY <666protik@gmail.com>
2026-06-03 04:59:46 +0000

    Create Copycat.cpp

---
+++ Copycat.cpp
@@ -0,0 +1,22 @@
+/*
+Read an integer variable and print it.
+
+Input: The input will contain an integer AA (−200000000<A<200000000−200000000<A<200000000).
+Output: Print the integer.
+
+Example:
+    Input: 2
+    Output: 2
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int A;
+	cin >> A;
+	cout << A;
+	return 0;
+}
